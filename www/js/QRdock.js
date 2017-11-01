3cc952dba0c462d15924d025361b4b4551474204
cnishaanntt <cnishaanntt@gmail.com>
2017-11-01 09:17:24 +0530

    Update QRdock.js

--- www/js/QRdock.js
+++ www/js/QRdock.js
@@ -20,8 +20,8 @@ function createDockPanel() {
     _dockPanel.closer.style.opacity = 0;
     _dockPanel.container.style.border.opacity = 0;
     
-    _dockPanel.container.style.bottom = "100px";
-    _dockPanel.container.style.right = "100px";
+    _dockPanel.container.style.bottom = "320px";
+    _dockPanel.container.style.right = "120px";
     
 
     _dockPanel.container.style.width = "auto";
